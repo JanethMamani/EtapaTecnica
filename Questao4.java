7bd82523f00c8c84bdd1f9e6a5d86350c6a695a1
JanethMamani <51747510+JanethMamani@users.noreply.github.com>
2021-02-08 11:18:08 -0300

    Add files via upload

---
+++ Questao4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Questao4{
+	public static void main(String[] args){
+		Scanner A = new Scanner(System.in);
+		Scanner B = new Scanner(System.in);
+		System.out.print(A.next());
+		System.out.print(A.next());
+	}
+}

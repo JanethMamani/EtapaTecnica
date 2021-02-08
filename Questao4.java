d7aeeddaa317d83b53575db078fdcc1a305511a9
JanethMamani <51747510+JanethMamani@users.noreply.github.com>
2021-02-08 11:20:41 -0300

    Update Questao4.java

--- Questao4.java
+++ Questao4.java
@@ -3,7 +3,7 @@ public class Questao4{
 	public static void main(String[] args){
 		Scanner A = new Scanner(System.in);
 		Scanner B = new Scanner(System.in);
-		System.out.print(A.next());
+		System.out.print(B.next());
 		System.out.print(A.next());
 	}
 }
